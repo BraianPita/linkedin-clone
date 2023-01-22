3271e0ceea690e5ae15fa4208a1dcaf62c2760f4
BraianPita <braianx100@gmail.com>
2023-01-21 19:46:09 -0600

    Integrated firebase UI, firestore, and firebase auth

---
+++ linkedin-clone/src/features/firebase/firebaseApp.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import firebaseConfig from "../../secrets/firebase.config";
+
+
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
